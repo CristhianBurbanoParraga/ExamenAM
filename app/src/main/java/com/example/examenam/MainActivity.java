f1df81d2c3072feb1ec5ac9f1baca5423402d4a6
CristhianBurbanoParraga <74200293+CristhianBurbanoParraga@users.noreply.github.com>
2021-03-11 17:46:13 -0500

    Examen commit v2

--- app/src/main/java/com/example/examenam/MainActivity.java
+++ app/src/main/java/com/example/examenam/MainActivity.java
@@ -21,7 +21,11 @@ public class MainActivity extends AppCompatActivity {
                 .setItemViewCacheSize(10)
                 .setLayoutManager(new GridLayoutManager(this, 3));
 
-
+        phvGallery
+                .addView(new GalleryImage(getApplicationContext(), "https://firebasestorage.googleapis.com/v0/b/artesanias-304016.appspot.com/o/images%2Faccesorios.jpg?alt=media&token=2e975a70-f3f5-4642-b130-47e8085e553d"))
+                .addView(new GalleryImage(getApplicationContext(), "https://firebasestorage.googleapis.com/v0/b/artesanias-304016.appspot.com/o/images%2Fartesana-as-en-madera.jpg?alt=media&token=c98ebc9b-cccf-41dd-8403-015b2c99e400"))
+                .addView(new GalleryImage(getApplicationContext(), "https://firebasestorage.googleapis.com/v0/b/artesanias-304016.appspot.com/o/images%2Fbolsos-artesanales-620x330.jpg?alt=media&token=c34ca617-b6f9-43af-a037-7f61570bfdc2"))
+                .addView(new GalleryImage(getApplicationContext(), "https://firebasestorage.googleapis.com/v0/b/artesanias-304016.appspot.com/o/images%2Fcaballomadera.jpg?alt=media&token=d4c7b249-218b-44d7-a4f8-fe2a9c90a097"));
 
 
     }
